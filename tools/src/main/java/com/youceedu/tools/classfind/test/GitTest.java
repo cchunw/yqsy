5f64bfc801bc01016b924abb9e46b57316445749
cchunw <429851079@qq.com>
2023-07-18 16:35:58 +0800

    hot-fix commit1

--- tools/src/main/java/com/youceedu/tools/classfind/test/GitTest.java
+++ tools/src/main/java/com/youceedu/tools/classfind/test/GitTest.java
@@ -1,19 +1,9 @@
 package com.youceedu.tools.classfind.test;
 
-import org.etsi.uri.x01903.v13.OCSPIdentifierType;
-
-/**
- * @program: yqsy
- * @ClassName GitTest
- * @Description TODO
- * @Author chen chunwei
- * @create: 2023-07-18 16:05
- * @Version 1.0
- */
 public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git3");
         System.out.println("hello git4");
-        System.out.println("master test1");
+        System.out.println("hot-fix test1");
     }
 }
