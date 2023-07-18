7d42fcf38c313b73ab52a34970ae43b35b4a00db
cchunw <429851079@qq.com>
2023-07-18 18:43:27 +0800

    master commit2

--- tools/src/main/java/com/youceedu/tools/classfind/test/GitTest.java
+++ tools/src/main/java/com/youceedu/tools/classfind/test/GitTest.java
@@ -5,6 +5,5 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git3");
         System.out.println("hello git4");
-        System.out.println("master test");
     }
 }
