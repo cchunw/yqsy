cc92ef4ae59e9e76437ce025daeebce93b20201d
cchunw <429851079@qq.com>
2023-07-18 16:15:14 +0800

    hot-fix commit2

--- tools/src/main/java/com/youceedu/tools/classfind/test/GitTest.java
+++ tools/src/main/java/com/youceedu/tools/classfind/test/GitTest.java
@@ -14,5 +14,6 @@ public class GitTest {
     public static void main(String[] args) {
         System.out.println("hello git3");
         System.out.println("hello git4");
+        System.out.println("master test1");
     }
 }
