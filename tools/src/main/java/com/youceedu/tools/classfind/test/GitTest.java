2290c85f4f0a5fe0f7dfbfec049b7ebc39734bb5
cchunw <139685094+cchunw@users.noreply.github.com>
2023-07-19 00:38:44 +0800

    pull commit

--- tools/src/main/java/com/youceedu/tools/classfind/test/GitTest.java
+++ tools/src/main/java/com/youceedu/tools/classfind/test/GitTest.java
@@ -10,5 +10,6 @@ public class GitTest {
         System.out.println("222222222222");
         System.out.println("1111111111111");
         System.out.println("push test");
+        System.out.println("pull test");
     }
 }
